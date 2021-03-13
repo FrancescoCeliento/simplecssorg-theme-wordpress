73764e35e46d65cd454f43795f60a042e3cd764d
Francesco <mail@francescoceliento.com>
2021-03-13 14:40:25 +0100

    Another bugfix

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer >
-	<p><a href="https://github.com/FrancescoCeliento/simplecssorg-theme-wordpress" target="_blank">SimpleCSS Theme Wordpress v0.4</a> is created by <a href="https://www.francescoceliento.com" target="_blank">Francesco Celiento</a> and is based on <a href="https://simplecss.org/" target="_blank"><b>Simple.css {}</b></a> (which is created by <a href="https://kevq.uk" target="_blank">Kev Quirk</a>)<br>
+	<p><a href="https://github.com/FrancescoCeliento/simplecssorg-theme-wordpress" target="_blank">SimpleCSS Theme Wordpress v0.4.1</a> is created by <a href="https://www.francescoceliento.com" target="_blank">Francesco Celiento</a> and is based on <a href="https://simplecss.org/" target="_blank"><b>Simple.css {}</b></a> (which is created by <a href="https://kevq.uk" target="_blank">Kev Quirk</a>)<br>
 	<a href="https://github.com/FrancescoCeliento/simplecssorg-theme-wordpress/archive/main.zip">Download this theme</a></p>
 	
 </footer>
